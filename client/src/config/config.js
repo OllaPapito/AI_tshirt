f949fa543680235ee981e6731273400ddc039df1
Adrian Aguiar <95946350+OllaPapito@users.noreply.github.com>
2023-05-09 10:40:41 -0300

    Add files via upload

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "https://ai-tshirt-6x94.onrender.com/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
